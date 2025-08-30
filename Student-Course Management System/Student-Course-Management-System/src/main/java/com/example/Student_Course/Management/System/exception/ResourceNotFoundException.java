2966f7304c4d87983f9841dff8dd140e9e1c91b0
pawan1211 <63227421+pawan1211@users.noreply.github.com>
2025-08-30 21:06:41 +0530

    Delete Student-Course Management System/Student-Course-Management-System directory

--- Student-Course Management System/Student-Course-Management-System/src/main/java/com/example/Student_Course/Management/System/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.Student_Course.Management.System.exception;
-
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String resource, Object id) {
-        super(resource + " not found with id " + id);
-    }
-}
